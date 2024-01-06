244d6a5aaad9827e2d8d266ada1c8999d7669d4b
Karthick Sriram <karthicksriram71@gmail.com>
2024-01-06 12:54:30 +0530

    fixed bugs

--- sage.py
+++ sage.py
@@ -7,37 +7,41 @@ g4f.debug.logging = False  # Enable debug logging
 g4f.debug.check_version = False  # Disable automatic version checking
 
 r = sr.Recognizer()
-source = sr.Microphone()
+source = sr.Microphone(0)
 engine = pyttsx3.init()
 
 def speak(text):
+    text = text[0:text.find("Some possible follow-up messages")]
     engine.say(text)
     engine.runAndWait()
 
-def callback(recognizer, audio):
-    try:
-        query = r.recognize_google(audio)
-        print(f"\nUser said: {query}")
-        response = g4f.ChatCompletion.create(
+def bing(query):
+    response = g4f.ChatCompletion.create(
         model="gpt-3.5-turbo",
         provider=g4f.Provider.Bing,
         messages=[{"role": "user", "content": query}],
-        )
-        print(response, flush=True, end='')
-        speak(response)
-        return query.lower()
+    )
+    print(response, flush=True, end='')
+    speak(response)
+
+speak("Hello! How can I help you?")
+while True:
+
+    try:
+            with source as s:
+                r.adjust_for_ambient_noise(s,duration=1)
+                print('Listening')
+                audio=r.listen(s)
+                query=r.recognize_google(audio)
+                print(f"\nUser said: {query}")
+                bing(query)
+                
     except sr.UnknownValueError:
-        pass
-    except Exception as e:
-       print("Prompt error: ", e)
+        source = sr.Microphone(0)
+        r = sr.Recognizer()
+    except KeyboardInterrupt:
+        speak("Goodbye!")
+        break
 
-def start_listening():
-    with source as s:
-        r.adjust_for_ambient_noise(s, duration=2)
-    print('Listening\n')
-    speak("Hello! How can I help you?")
-    stop_listening = r.listen_in_background(source, callback)
-    while True:
-        time.sleep(0.5)
 
-start_listening()
+
